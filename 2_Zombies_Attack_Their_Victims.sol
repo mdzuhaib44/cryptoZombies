aad399db9f8c9d7a1853bd48985b1d0c92a0364b
Zuhaib Mohammed <92208957+mdzuhaib44@users.noreply.github.com>
2021-10-16 14:56:42 +0530

    Create 2_Zombies_Attack_Their_Victims.sol

---
+++ 2_Zombies_Attack_Their_Victims.sol
@@ -0,0 +1,42 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+//Import a smart contract
+import "./2_Making_The_Zombie_Factory.sol";
+
+//Define an Interface of a Smart Contract
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+contract ZombieFeeding is ZombieFactory { //Inherit the properties of ZombieFactory
+
+  //contract address where the Crypto Kitties in hosted
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]); //check if 
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    _createZombie("Catu", newDna); //_createZombie should be made internal
+  }
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId); //return the only output we need
+    feedAndMultiply(_zombieId, kittyDna);
+  }
+
+}
